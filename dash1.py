976e68da2516d82af943204edd464d25caf4739c
Aradanius <95956288+Aradanius@users.noreply.github.com>
2023-11-05 16:24:38 +0800

    Add files via upload

--- dash1.py
+++ dash1.py
@@ -1,7 +1,6 @@
 from dash import Dash, html, dcc, callback, Output, Input
 import plotly.express as px
 import pandas as pd
-import dash_bootstrap_components as dbc
 import dash_draggable
 
 df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
